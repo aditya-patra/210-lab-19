4c0a7d4e187ffa26e19d7cc916e112288f30e677
Aditya Patra <adityabpatra@gmail.com>
2025-10-04 15:01:38 -0700

    add file reading

--- main.cpp
+++ main.cpp
@@ -9,6 +9,8 @@ Create vector of movies that have been reviewed from file of movie comments
 */
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 using namespace std;
 
 const int SIZE = 7;  
@@ -28,10 +30,16 @@ class Movies {
             name = title;
             head = nullptr;
         } 
+
         string getName() {
             return name;
         }
-        Review addHead(float rating, string comment) {
+
+        void setName(string newName) {
+            name = newName;
+        }
+
+        void addHead(float rating, string comment) {
             Review * temp = new Review;
             if (!head) {
                 head = temp;
@@ -46,76 +54,53 @@ class Movies {
                 head = temp;
             }   
         }
+
+        void printMovieReviews() {
+            cout << "Movie: " << name << endl;
+            if (head == nullptr) {
+                return;
+            }
+            int i = 0;
+            float sum = 0;
+            while (head != nullptr) {
+                i++;
+                sum += head->rating;
+                cout << "Review " << i << ": " << endl;
+                cout << "Rating: " << head->rating << endl;
+                cout << "Comments: " << head->comment << endl << endl;
+                head = head->next;
+            }
+            cout << endl << "Average Rating: " << (sum / i) << endl;    
+        }
+
+        void deleteList() {
+            Review * temp;
+            while (head != nullptr) {
+                temp = head;
+                head = head->next;
+                delete temp;
+            }
+            head = nullptr;
+        }
 };
 
-void printList(Review *);
-void addHead(Review * &, float, string);
-void deleteList(Review *);
 
 int main() {
     Review *head = nullptr;
+    string name;
     float rating;
     string comment;
-    string dummy; //flush input
-    string cont;
-    while (true) {
-        cout << "Enter your rating of the movie: ";
-        cin >> rating;
-        cout << "Enter your review of the movie: ";
-        getline(cin, dummy);
-        getline(cin, comment);
-        addHead(head, rating, comment);
-        cout << "Would you like to enter another review?(Y/n) ";
-        cin >> cont;
-        if (cont == "n") {
-            break;
+    ifstream file("Movie1.txt");
+    string line;
+    vector<Movies> movies;
+    if(file.good()) {
+        getline(file, line);
+        name = line;
+        for (int i = 0; i < 3; i++) {
+            getline(file, line);
+            comment = line;
         }
     }
-    printList(head);
-    deleteList(head);
 
     return 0;
-}
-
-void printList(Review * head) {
-    if (head == nullptr) {
-        return;
-    }
-    int i = 0;
-    float sum = 0;
-    while (head != nullptr) {
-        i++;
-        sum += head->rating;
-        cout << "Review " << i << ": " << endl;
-        cout << "Rating: " << head->rating << endl;
-        cout << "Comments: " << head->comment << endl << endl;
-        head = head->next;
-    }
-    cout << endl << "Average Rating: " << (sum / i) << endl;
-}
-
-void addHead(Review * &head, float rating, string comment) {
-    Review * temp = new Review;
-    if (!head) {
-        head = temp;
-        temp->next = nullptr;
-        temp->rating = rating;
-        temp->comment = comment;
-    }
-    else {
-        temp->next = head;
-        temp->rating = rating;
-        temp->comment = comment;
-        head = temp;
-    }
-}
-
-void deleteList(Review * head) {
-    Review * temp;
-    while (head != nullptr) {
-        temp = head;
-        head = head->next;
-        delete temp;
-    }
-    head = nullptr;
 }
